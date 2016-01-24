f92dee8bf89b939edf39c56d92979959cc452895
tk baylis <tammy.baylis@gmail.com>
2016-01-23 18:52:31 -0800

    First commit

---
+++ always_face_camera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class always_face_camera : MonoBehaviour {
+
+    // From Unity Community and Wiki
+    // http://answers.unity3d.com/questions/181000/gui-text-always-facing-camera.html
+    // http://wiki.unity3d.com/index.php?title=CameraFacingBillboard
+    public Camera cameraToLookAt;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 v = cameraToLookAt.transform.position - transform.position;
+		v.x = v.z = 0.0f;
+		transform.LookAt( cameraToLookAt.transform.position - v ); 
+		transform.Rotate(0,180,0);
+	}
+}
---
+++ show_panel_hide_other_pins.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class show_panel_hide_other_pins : MonoBehaviour {
+
+    public GameObject[] allPins;
+    public GameObject panelGroup;
+
+	// Use this for initialization
+	void Start () {
+
+        // Instantiate all pins
+        allPins = GameObject.FindGameObjectsWithTag("pin");  // this might be too slow
+
+        // Hide panel group, if any
+        if (panelGroup == null)
+        {
+            return;
+        }
+        else
+        {
+            panelGroup.SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseEnter()
+    {
+        // Hide all other pins
+        foreach (GameObject pin in allPins)
+        {
+            if (pin.name != gameObject.name)
+            {
+
+                pin.SetActive(false);
+            }
+        }
+
+        // Display panel group, if any
+        if (panelGroup == null)
+        {
+            return;
+        }
+        else
+        {
+            panelGroup.SetActive(true);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        // Show all pins again
+        foreach (GameObject pin in allPins)
+        {
+            pin.SetActive(true);
+        }
+
+        // Hide panel group, if any
+        if (panelGroup == null)
+        {
+            return;
+        }
+        else
+        {
+            panelGroup.SetActive(false);
+        }
+    }
+
+    void OnMouseDown()
+    {
+
+    }
+}
