85790506bdb1c756f0f7ba1c588916f65f35dbca
tk baylis <tammy.baylis@gmail.com>
2016-01-25 15:04:32 -0800

    Show panel, hide pins on click instead mouseover

---
+++ hide_button.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hide_button : MonoBehaviour {
+
+    public GameObject[] allPins;
+    public GameObject featuresPanel;
+
+    // Use this for initialization
+    void Start () {
+        // Instantiate all pins
+        allPins = GameObject.FindGameObjectsWithTag("pin");  // this might be too slow
+
+        // Get parent panel if not provided?  // !!!
+        //featuresPanel = gameObject.transform.parent;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnMouseDown()
+    {
+
+        Debug.Log("THe button was pressed");
+
+        // Show all pins again
+        foreach (GameObject pin in allPins)
+        {
+            pin.SetActive(true);
+        }
+
+        // Hide feature panel, if any
+        if (!(featuresPanel == null))
+            featuresPanel.SetActive(false);
+    }
+}
--- show_panel_hide_other_pins.cs
+++ show_panel_hide_other_pins.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class show_panel_hide_other_pins : MonoBehaviour {
 
     public GameObject[] allPins;
-    public GameObject panelGroup;
+    public GameObject previewPanel;
+    public GameObject featuresPanel;
 
 	// Use this for initialization
 	void Start () {
@@ -12,66 +13,44 @@ public class show_panel_hide_other_pins : MonoBehaviour {
         // Instantiate all pins
         allPins = GameObject.FindGameObjectsWithTag("pin");  // this might be too slow
 
+        // Hide preview panel, if any
+        if (!(previewPanel == null))
+            previewPanel.SetActive(false);
+
         // Hide panel group, if any
-        if (panelGroup == null)
-        {
-            return;
-        }
-        else
-        {
-            panelGroup.SetActive(false);
-        }
+        if (!(featuresPanel == null))
+            featuresPanel.SetActive(false);
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
     void OnMouseEnter()
     {
-        // Hide all other pins
-        foreach (GameObject pin in allPins)
-        {
-            if (pin.name != gameObject.name)
-            {
-
-                pin.SetActive(false);
-            }
-        }
-
-        // Display panel group, if any
-        if (panelGroup == null)
-        {
-            return;
-        }
-        else
-        {
-            panelGroup.SetActive(true);
-        }
+        // Display preview panel, if any
+        if (!(previewPanel == null))
+            previewPanel.SetActive(true);
     }
 
     void OnMouseExit()
     {
-        // Show all pins again
-        foreach (GameObject pin in allPins)
-        {
-            pin.SetActive(true);
-        }
-
-        // Hide panel group, if any
-        if (panelGroup == null)
-        {
-            return;
-        }
-        else
-        {
-            panelGroup.SetActive(false);
-        }
+        // Hide preview panel, if any
+        if (!(previewPanel == null))
+            previewPanel.SetActive(false);
     }
 
     void OnMouseDown()
     {
+        // Hide all other pins
+        foreach (GameObject pin in allPins)
+        {
+            if (pin.name != gameObject.name)        
+                pin.SetActive(false);
+        }
 
+        // Display panel group, if any
+        if (!(featuresPanel == null))
+            featuresPanel.SetActive(true);
     }
 }
