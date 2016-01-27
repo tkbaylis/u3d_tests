618ccc069b083335f04f5c648e133c4706cbbba9
tk baylis <tammy.baylis@gmail.com>
2016-01-27 14:08:09 -0800

    Refactor: 3 classes to AllChildren class

---
+++ AllChildren.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AllChildren : MonoBehaviour {
+
+    public void showAll()
+    {
+        foreach (Transform child in gameObject.transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+
+    public void hideAll()
+    {
+        foreach (Transform child in gameObject.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+}
